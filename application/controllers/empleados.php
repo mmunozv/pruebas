b042c793c2dc877ed23f5f07286c17eccd5ac74b
marcelo oficina <marcelomv@ccgg.cl>
2013-07-02 14:24:01 -0400

    paginacion en prueba

--- application/controllers/empleados.php
+++ application/controllers/empleados.php
@@ -4,14 +4,28 @@ class Empleados extends CI_Controller {
 	public function __construct()
 	{
 		parent::__construct();
+		$this->load->model('personal/personal_model');
 	}
 
 	public function ver_personal()
 	{
-		$this->load->model('personal/personal_model');
-		$data['obtener_personal'] = $this->personal_model->traer_empleados();
 
+	    $this->load->library('pagination');
+	 
+	    $opciones = array();
+	    $desde = ($this->uri->segment(3)) ? $this->uri->segment(3) : 0;
+	 
+	    $opciones['per_page'] = 20;
+	    $opciones['base_url'] = base_url().'index.php/empleados/ver_personal';
+	    $opciones['total_rows'] = $this->personal_model->filas();
+	    $opciones['uri_segment'] = 3;
+	 
+	    $this->pagination->initialize($opciones);
+	 
+	    $data['paginacion'] = $this->pagination->create_links();
+		$data['obtener_personal'] = $this->personal_model->traer_empleados($opciones['per_page'],$desde);
 		$this->load->view('personal/personal_view',$data);
+
 	}
 
 
